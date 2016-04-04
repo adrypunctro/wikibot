1b657e0484ede581fbf0886cf986c185fceac3dd
Adrian Simionescu <adrypunctro@yahoo.com>
2016-04-04 04:18:49 +0300

    Add a python web server

--- web/web.py
+++ web/web.py
@@ -1,30 +1,54 @@
-# app.py
-
-
 from flask import Flask
-from flask import request, render_template
-from flask.ext.sqlalchemy import SQLAlchemy
-from config import BaseConfig
+from flask import request
+from flask import jsonify
+from pymongo import MongoClient
+import os
 
+#### CONFIG ##############################
+
+#
+db_host = os.environ.get('MONGO_PORT_27017_TCP_ADDR', '127.0.0.1')
+db_port = 27017
+
+##########################################
 
 app = Flask(__name__)
-app.config.from_object(BaseConfig)
-db = SQLAlchemy(app)
 
-
-from models import *
-
+# MongoDb connect
+client = MongoClient(db_host, db_port)
+db = client.wiki_app
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        text = request.form['text']
-        post = Post(text)
-        db.session.add(post)
-        db.session.commit()
-    posts = Post.query.order_by(Post.date_posted.desc()).all()
-    return render_template('index.html', posts=posts)
+    if request.method == 'GET':
+        day = request.args.get('day')
+        year = request.args.get('year')
+        category = request.args.get('category')
+        keywords = request.args.get('keywords')
+
+        condition = {}
+        if day is not None:
+            condition['day'] = day
+        if year is not None:
+            condition['year'] = year
+        if category is not None:
+            condition['category'] = category
+        if keywords is not None:
+            condition['$text'] = {'$search': keywords}
+        
+        cursor = db.articles.find(condition)
+        results = []
+        for document in cursor:
+            results.append( {'year':document['year'],
+                             'day':document['day'],
+                             'category':document['category'],
+                             'title':document['title']} );
+        
+        return jsonify({ 'results': results })
+    else:
+        return "Not valid!"
+    
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
